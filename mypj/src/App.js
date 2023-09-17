e894e183c1ca61b3780bf1fdfce216d4128bba54
edehgoodness123 <goodnessgift022@gmail.com>
2023-09-17 14:13:02 +0100

    add file

---
+++ mypj/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import WithHeaderAndQuoteExample from './components/Cards';
+import BasicExample from './components/Navbar';
+
+
+const App = () => {
+  return (
+    <div>
+        <h2>welcome to react</h2>
+        <BasicExample> </BasicExample>
+        <WithHeaderAndQuoteExample> </WithHeaderAndQuoteExample>
+    </div>
+)
+};
+
+export default App;
+
+
